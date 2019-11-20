b11c3636bd1be45fdf414563fed1095a1d58d1f0
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2019-11-20 12:28:40 +0530

    multi-process
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- static/src/static.c
+++ static/src/static.c
@@ -2,11 +2,19 @@
 #include "str.h"
 
 int main() {
-    while(1){
-       unsigned int i = 0;
-       printf("======================================Within main=================================== \n");
-       get_string();
-       while( i < 1000000) i++;
-    }
-     return 0;
+    for(int i=0;i<5;i++) // loop will run n times (n=5) 
+    { 
+        if(fork() == 0) 
+        { 
+            printf("===========================================[son] pid %d from [parent] pid %d\n",getpid(),getppid()); 
+            while(1){
+               unsigned int i = 0;
+               while( i < 1000000) i++;
+            }
+            exit(0); 
+        } 
+    } 
+    for(int i=0;i<5;i++) // loop will run n times (n=5) 
+        wait(NULL); 
+    return 0;
 }
